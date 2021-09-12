1e33ef89ce1eb7c66ef55ac46eae615c4f2e6ead
Ryan <ryanzzheng@gmail.com>
2021-09-12 01:13:13 -0400

    Added Start Date End Date parameters

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
 const MainPage = () => {
-    const [imageJSON, setImageJSON] = useState({});
+    const [imageJSONList, setImageJSON] = useState([]);
 
-    const getImageJSONFromNASA = async () => {
-        const { data } = await axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`);
-        setImageJSON(data);
+    const getImageJSONFromNASA = async (startDate, endDate) => {
+        let nasaAPI = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`;
+        let dataList = [];
+        if (startDate && endDate) {
+            const dateRange = `&start_date=${startDate.format('YYYY-MM-DD')}&end_date=${endDate.format('YYYY-MM-DD')}`;
+
+            nasaAPI = nasaAPI.concat(dateRange);
+            debugger;
+            const { data } = await axios.get(nasaAPI);
+            dataList = data;
+        } else {
+            const { data } = await axios.get(nasaAPI);
+            dataList.push(data);
+        }
+
+
+        setImageJSON(dataList);
     }
 
     useEffect(() => {
-        getImageJSONFromNASA();
+        getImageJSONFromNASA(null, null);
     }, []);
 
     return (
         <div>
-            <span>{imageJSON.date}</span>
-            <span>{imageJSON.explanation}</span>
-            <span>{imageJSON.hdurl}</span>
-            <span>{imageJSON.title}</span>
+            {imageJSONList.length > 0 && imageJSONList.map((imageJSON, index) => {
+                return (
+                    <div key={index}>
+                        <span>{imageJSON.date}</span>
+                        <span>{imageJSON.explanation}</span>
+                        <span>{imageJSON.hdurl}</span>
+                        <span>{imageJSON.title}</span>
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
