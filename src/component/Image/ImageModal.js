c9cb0a093cf8767d978d3721a911c32ddea80895
Ryan <ryanzzheng@gmail.com>
2021-09-13 16:01:11 -0400

    Added ability to view a larger size of the picture

--- src/component/Image/ImageModal.js
+++ src/component/Image/ImageModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Typography,
     Card,
@@ -7,6 +7,8 @@ import {
     CardActions,
     CardContent,
     IconButton,
+    Modal,
+    Fade
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
@@ -33,43 +35,81 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     content: {
-        height: 285
+        height: 320
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        width: 1280,
+        height: 720
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    hdimage: {
+        height: '100%',
+        width: '100%'
     }
 }));
 
 const ImageModal = ({ image }) => {
     const classes = useStyles();
+    const [isOpen, setIsOpen] = useState(false);
 
     const handleMediaClicked = () => {
+        setIsOpen(true);
+    }
 
+    const handleClose = () => {
+        setIsOpen(false);
     }
 
     return (
-        <Card className={classes.root}>
-            <CardActionArea onClick={handleMediaClicked}>
-                <CardMedia
-                    className={classes.media}
-                    image={image.url}
-                    title={image.title}
-                />
-            </CardActionArea>
-            <CardContent className={classes.content}>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {image.title}
-                </Typography>
-                <Typography className={classes.explanation} variant="body2" color="textSecondary" component="p">
-                    {image.explanation}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <IconButton>
-                    <FavoriteBorderRoundedIcon />
-                </IconButton>
-                <IconButton>
-                    <ShareIcon />
-                </IconButton>
-            </CardActions>
-        </Card>
+        <>
+            <Card className={classes.root}>
+                <CardActionArea onClick={handleMediaClicked}>
+                    <CardMedia
+                        className={classes.media}
+                        image={image.url}
+                        title={image.title}
+                    />
+                </CardActionArea>
+                <CardContent className={classes.content}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {image.title}
+                    </Typography>
+                    <Typography gutterBottom variant="body2" color="textSecondary" component='p'>
+                        {image.date}
+                    </Typography>
+                    <Typography className={classes.explanation} variant="body2" color="textSecondary" component="p">
+                        {image.explanation}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <IconButton>
+                        <FavoriteBorderRoundedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ShareIcon />
+                    </IconButton>
+                </CardActions>
+            </Card>
+            <Modal
+                className={classes.modal}
+                open={isOpen}
+                onClose={handleClose}
+                closeAfterTransition
+            >
+                <Fade in={isOpen}>
+                    <div className={classes.paper}>
+                        <img className={classes.hdimage} src={image.hdurl} alt="" />
+                    </div>
+                </Fade>
+            </Modal>
+        </>
     )
 }
 
